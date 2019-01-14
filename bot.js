60f8af6e088231f48b8f3dabf72d5b97cc893a04
krapa936 <46627271+krapa936@users.noreply.github.com>
2019-01-14 17:51:17 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,6 +30,30 @@ client.user.setGame(`by Floki | %help`,"http://twitch.tv/S-F")
 
 	
 
+const client = new Discord.Client();
+var prefix = "*&";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : sent to`); 
+ message.delete(); 
+};     
+});
+
+
+
+
+
+
+
+
+
 
 
 
